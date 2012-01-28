d1f3ef081f5de5601236193b05cf18bebcdccdaf
Lars Funke <larsfunke1996@gmail.com>
2012-01-28 13:23:32 +0100

    added functionality to rectangle

--- src/util/Rectangle2D.java
+++ src/util/Rectangle2D.java
@@ -7,5 +7,25 @@ public class Rectangle2D {
 	public Rectangle2D(Vector2D topleft, Vector2D bottomright){
 		this.topleft = topleft;
 		this.bottomright = bottomright;
+		
+		if(topleft.x > bottomright.x) {
+			this.topleft.x = bottomright.x;
+			this.bottomright.x = topleft.x;
+		}
+		
+		if(topleft.y < bottomright.y) {
+			this.topleft.y = bottomright.y;
+			this.bottomright.y = topleft.y;
+		}
+	}
+	
+	public double top() {return topleft.y;}
+	public double bottom() {return bottomright.y;}
+	public double right() {return bottomright.x;}
+	public double left() {return topleft.x;}
+	
+	public boolean inside(Vector2D p) {
+		return Util.isValueInBounds(topleft.x, p.x, bottomright.x) &&
+				Util.isValueInBounds(bottomright.y, p.y, topleft.y);
 	}
 }
