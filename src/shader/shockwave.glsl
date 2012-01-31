363d65f0f24e867e7697aa81e6e0167d1ed0d106
Lars Funke <larsfunke1996@gmail.com>
2012-01-31 23:26:30 +0100

    Licensing. added notices and copy of gpl3

--- src/shader/shockwave.glsl
+++ src/shader/shockwave.glsl
@@ -1,4 +1,21 @@
- 
+/**
+ * Copyright (c) 2011-2012 Henning Funke.
+ * 
+ * This file is part of Battlepath.
+ *
+ * Battlepath is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Battlepath is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
  uniform vec2 pos[10];
  uniform float radius[10];
  uniform int number;
