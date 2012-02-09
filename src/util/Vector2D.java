9b087e920761377b406a2682e6dd6ecd45d15001
Lars Funke <larsfunke1996@gmail.com>
2012-02-09 18:31:43 +0100

    Quintupled vector length performance. (5.3E-5ms to 1.1E-5ms per exec on my sys)

--- src/util/Vector2D.java
+++ src/util/Vector2D.java
@@ -58,7 +58,7 @@ public class Vector2D {
 	}
 	
 	public double length() {
-		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+		return Math.sqrt(x*x + y*y);
 	}
 	
 	public double squaredLength() {
